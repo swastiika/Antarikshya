f9c2ffac7429d4e4602a8dd4d007ee0b708c30ec
sostika <sostika002@gmail.com>
2025-02-15 22:26:37 +0545

    eee

--- spacemod/src/App.js
+++ spacemod/src/App.js
@@ -1,18 +1,43 @@
 import './App.css';
-import Header  from './header';
-import React,{useEffect} from 'react';
-import TwinklingStars from './twinkle';
+import Header from './components/header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import TwinklingStars from './components/twinkle';
+import Antarikshya from "./components/start";
+import { Link, Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+     
+      <Outlet /> 
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className='button-container'>
+       <TwinklingStars /> 
+      <Link to={"/antarikshya"}>
+        <button className='buttn'>Start</button>
+      </Link>
+      <p className='text-1'>Let me take you to our Antarikshya</p>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <>
-    <Header/>
-    <TwinklingStars/>
-   
-   <div className='button-container'>
-    <button className='buttn'>Start</button>
-   </div>
-   
-   </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="antarikshya" element={<Antarikshya />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ spacemod/src/components/start.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./header";
+import "./start.css"
+function Antarikshya() {
+    return (
+        <div className="mikecheck">
+        </div>
+    );
+}
+
+export default Antarikshya;
